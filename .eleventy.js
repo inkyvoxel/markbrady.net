b824eedcb74b00413e50729f98d84d3255381529
inkyvoxel <hello@inkyvoxel.com>
2024-05-31 11:48:38 +0100

    Change structure to match my other 11ty sites

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+module.exports = function (eleventyConfig) {
+	eleventyConfig.addPassthroughCopy({ './src/robots.txt': '/robots.txt' });
+
+	return {
+		dir: {
+			input: "src",
+		},
+		passthroughFileCopy: true,
+	};
+}
