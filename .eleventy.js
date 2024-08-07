7a713635cb5b0d278f55e9eacd0e5d74cb28e578
inkyvoxel <hello@inkyvoxel.com>
2024-08-07 08:57:06 +0100

    Add sitemap

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,11 @@
+import { DateTime } from "luxon";
+
 export default function (eleventyConfig) {
+
+  eleventyConfig.addFilter("isoDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toISO();
+  });
+
   eleventyConfig.addPassthroughCopy({ "./src/robots.txt": "/robots.txt" });
 
   return {
