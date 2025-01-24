3d93cbdeb9b889ef84c1703cae3f237bc3fe2970
inkyvoxel <hello@inkyvoxel.com>
2025-01-24 09:14:59 +0000

    New year! Trying out Prettier

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,6 @@
 import { DateTime } from "luxon";
 
 export default function (eleventyConfig) {
-
   eleventyConfig.addFilter("isoDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toISO();
   });
